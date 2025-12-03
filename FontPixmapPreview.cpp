8de9820e6b393efb0c4b2116902aa5a5acb604b5
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2025-12-03 13:13:59 +0100

    Show scrollbars if preview gets too large

--- FontPixmapPreview.cpp
+++ FontPixmapPreview.cpp
@@ -6,8 +6,8 @@ void FontPixmapPreview::paintEvent(QPaintEvent *e) {
     QPainter p(this);
     p.setPen(Qt::NoPen);
     p.drawPicture(0, 0, _picture);
-
     p.end();
+    setMinimumSize(_picture.boundingRect().size());
 }
 
 void FontPixmapPreview::setPicture(const QPicture &pic) {
